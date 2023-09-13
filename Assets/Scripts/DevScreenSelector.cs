663fded3e001f3df40261210a15811cd2c0bdb50
Vohmu <113022042+Vohmu@users.noreply.github.com>
2023-09-13 10:46:03 +0200

    UI DevScreenSelector
    Dropdown in UI to enable/disable UI dev screens

---
+++ Assets/Scripts/DevScreenSelector.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DevScreenSelector : MonoBehaviour
+{
+    [SerializeField] private GameObject[] devScreens;
+    
+    // enum Devs { Joep, Kizi, Marco, Martijn, Max, Nathan, Nick, Sander, Seana }
+    // [SerializeField] private Devs startingDev;
+
+    // private void Awake()
+    // {
+    //     Debug.Log((int)startingDev);
+    //     SelectDevScreen((int)startingDev);
+    // }
+
+    public void SelectDevScreen(int index)
+    {
+        for (int i = 0; i < devScreens.Length; i++)
+        {
+            if (i == index)
+            {
+                devScreens[i].SetActive(true);
+            }
+            else
+                devScreens[i].SetActive(false);
+        }
+    }
+}
