0e8f1dd5545035e6eee51c6cc600117a5a79711e
Mackthis <s.a.m.buitenhuis@students.uu.nl>
2023-09-20 19:14:04 +0200

    Full implementation ChunkGrid spawning

--- Assets/Scripts/ChunkGrid/ChunkGridBoundingBoxBehaviour.cs
+++ Assets/Scripts/ChunkGrid/ChunkGridBoundingBoxBehaviour.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class ChunkGridBoundingBoxBehaviour : BoundingBoxBehaviour
 {
+    public Vector3 offsetChild;
+    public Vector3 scaleChild;
+
     protected override void SetLocalTransform()
     {
         IChunkGridSize chunkGridSize = childObject.GetComponent(typeof(IChunkGridSize)) as IChunkGridSize;
         (int gridSizeX, int gridSizeY, int gridSizeZ) = chunkGridSize.GridSize;
         Vector3 gridSize = new(gridSizeX, gridSizeY, gridSizeZ);
         
-        base.SetLocalTransform();
         Transform childTransform = childObject.transform;
         childTransform.SetLocalPositionAndRotation(new Vector3(-0.5f, -0.5f, -0.5f), Quaternion.identity);
         childTransform.localScale = new Vector3(1 / gridSize.x, 1 / gridSize.y, 1 / gridSize.z);
+    }
+
+    protected void Update()
+    {
     }
 }
