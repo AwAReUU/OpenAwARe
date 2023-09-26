37eca3897c4508f7ada8f23b0afa4210a1996dce
36mb <74769682+36mb@users.noreply.github.com>
2023-09-26 11:46:24 +0200

    start of ingredients ui

--- Assets/Scripts/ListsOverviewScreen.cs
+++ Assets/Scripts/ListsOverviewScreen.cs
@@ -14,9 +14,14 @@ public class ListsOverviewScreen : MonoBehaviour
 
     // (assigned within unity)
     [SerializeField] private GameObject addListButton;
-    [SerializeField] private GameObject deleteButton;
+    [SerializeField] private GameObject deleteButtonObject;
     [SerializeField] private GameObject listObject;
 
+    [SerializeField] private GameObject scrollView;
+    [SerializeField] private Transform scrollViewContent;
+
+    [SerializeField] private GameObject listItemObject;
+ 
     private void OnEnable()
     {
         DisplayLists();
@@ -32,31 +37,53 @@ public class ListsOverviewScreen : MonoBehaviour
         RemoveListObjects();
 
         float objectDist = 50; // distance between list objects
-        float yPos;
 
+        Debug.Log(listObject.GetComponent<RectTransform>().sizeDelta);
+  
+        Debug.Log(listItemObject.GetComponent<RectTransform>().sizeDelta);
+
+        Vector2 listObjectSize = listItemObject.GetComponent<RectTransform>().sizeDelta;
+        Vector2 scrollViewSize = scrollView.GetComponent<RectTransform>().sizeDelta;
+        Vector2 scrollViewPos = scrollView.GetComponent<RectTransform>().anchoredPosition;
+        Debug.Log("scrollviewpos " + scrollViewPos);
         for (int i = 0; i < ingredientListManager.ingredientLists.Count; i++)
         {
-            // calculate height for list object
-            yPos = listObject.transform.localPosition.y - (listObject.GetComponent<RectTransform>().sizeDelta.y + objectDist) * i;
+            Button ingredientList = GameObject.Find("IngredientList").GetComponent<Button>();
+            Button delBtn = GameObject.Find("DeleteButton").GetComponent<Button>();
+
+            Vector3 listItemPosition = new Vector3
+            {
+                x = scrollViewPos.x,
+                y = listObject.transform.localPosition.y - (listObjectSize.y + objectDist) * i,
+                z = 0
+            };
 
             // create a new list item to display this list
-            GameObject newItem = Instantiate(listObject, listObject.transform.parent);
-            newItem.GetComponent<RectTransform>().localPosition = new Vector3(listObject.transform.localPosition.x, yPos, 0); // set item position to the correct height
+            GameObject newItem = Instantiate(listItemObject, scrollViewContent);
+            newItem.GetComponent<RectTransform>().localPosition = listItemPosition;
+
             newItem.SetActive(true);
 
             // change the text to match the list info
-            newItem.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text = ingredientListManager.ingredientLists[i].listName;
-            newItem.transform.GetChild(1).GetComponent<TMPro.TextMeshProUGUI>().text = ingredientListManager.ingredientLists[i].NumberOfIngredients().ToString();
-            int itemIndex = i;
-            newItem.transform.GetComponent<Button>().onClick.AddListener(() => { OnListButtonClick(itemIndex); });
-            listObjects.Add(newItem);
+            //newItem.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text = ingredientListManager.ingredientLists[i].listName;
+            //newItem.transform.GetChild(1).GetComponent<TMPro.TextMeshProUGUI>().text = ingredientListManager.ingredientLists[i].NumberOfIngredients().ToString();
+            //int itemIndex = i;
+            //newItem.transform.GetComponent<Button>().onClick.AddListener(() => { OnListButtonClick(itemIndex); });
+            //listObjects.Add(newItem);
 
             // create a deleteButton for this list
-            GameObject newButton = Instantiate(deleteButton, deleteButton.transform.parent);
-            newButton.GetComponent<RectTransform>().localPosition = new Vector3(deleteButton.transform.localPosition.x, yPos, 0); // set item position to the correct height
-            newButton.SetActive(true);
-            newButton.transform.GetComponent<Button>().onClick.AddListener(() => { OnDeleteButtonClick(itemIndex); });
-            listObjects.Add(newButton);
+            Button btn = GameObject.Find("DeleteButton").GetComponent<Button>();
+            //GameObject deleteButton = Instantiate(deleteButtonObject, scrollViewContent);
+            //Vector3 deleteButtonPosition = new Vector3
+            //{
+            //    x = listItemPosition.x + (listObjectSize.x / 2),
+            //    y = listItemPosition.y,
+            //    z = listItemPosition.z
+            //};
+            //deleteButton.GetComponent<RectTransform>().localPosition = deleteButtonPosition;
+            //deleteButton.SetActive(true);
+            //deleteButton.transform.GetComponent<Button>().onClick.AddListener(() => { OnDeleteButtonClick(itemIndex); });
+            //listObjects.Add(deleteButton);
         }
     }
 
