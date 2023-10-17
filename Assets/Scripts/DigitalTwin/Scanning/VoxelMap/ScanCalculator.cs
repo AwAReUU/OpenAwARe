6678816f7b300fee524ffb3cd06996520c511a16
Mackthis <s.a.m.buitenhuis@students.uu.nl>
2023-10-17 12:00:51 +0200

    Refoldering, namespacing, setup

---
+++ Assets/Scripts/DigitalTwin/Scanning/VoxelMap/ScanCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AwARe.DataStructures;
+
+namespace AwARe.DigitalTwin.VoxelMap
+{
+    public class ScanCalculator : IScanCalculator
+    {
+        public void UpdateScanInfo(ARInfo arInfo, IChunkGrid<ScanInfo> scanInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Assets/Scripts/General/Support/Visualizers/Special/GroupVisualizers.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+namespace AwARe.Visualization
+{
+    public class GroupVisualizer<In> : IVisualizer<In[]>
+    {
+        IVisualizer<In>[] visualizers;
+
+        public GroupVisualizer(IVisualizer<In>[] visualizers)
+        {
+            this.visualizers = visualizers;
+        }
+
+        public void Visualize(In[] toShow)
+        {
+            int l = Math.Max(toShow.Length, visualizers.Length);
+            for (int i = 0; i < l; i++)
+                visualizers[i].Visualize(toShow[i]);
+        }
+
+        public void Visualize()
+        {
+            for (int i = 0; i < visualizers.Length; i++)
+                visualizers[i].Visualize();
+        }
+    }
+
+    public class TrackableCollectionVisualizer<In> : IVisualizer<TrackableCollection<In>>
+    {
+        IVisualizer<In>[] visualizers;
+
+        public TrackableCollectionVisualizer(IVisualizer<In>[] visualizers)
+        {
+            this.visualizers = visualizers;
+        }
+
+        public void Visualize(TrackableCollection<In> toShow)
+        {
+            int l = visualizers.Length;
+            Debug.Log("TrackableCollectionV: " + toShow.count + " , " + l);
+            int i = -1;
+            foreach (var showable in toShow)
+            {
+                if (++i >= l) break;
+                visualizers[i].Visualize(showable);
+            }
+        }
+        public void Visualize()
+        {
+            for (int i = 0; i < visualizers.Length; i++)
+                visualizers[i].Visualize();
+        }
+    }
+
+    public class IgnoreOnNullVisualizer<In> : IVisualizer<In?>
+        where In : struct
+    {
+        IVisualizer<In> visualizer;
+
+        public IgnoreOnNullVisualizer(IVisualizer<In> visualizer)
+        {
+            this.visualizer = visualizer;
+        }
+
+        public void Visualize(In? toShow)
+        {
+            if (!toShow.HasValue)
+                return;
+            visualizer.Visualize(toShow.Value);
+        }
+
+        public void Visualize() => visualizer.Visualize();
+    }
+
+    public class HideOnNullVisualizer<In> : IVisualizer<In?>
+        where In : struct
+    {
+        IVisualizer<In> visualizer;
+
+        public HideOnNullVisualizer(IVisualizer<In> visualizer)
+        {
+            this.visualizer = visualizer;
+        }
+
+        public void Visualize(In? toShow)
+        {
+            Debug.Log("HideOnNullV");
+            if (!toShow.HasValue)
+            {
+                visualizer.Visualize();
+                return;
+            }
+
+            visualizer.Visualize(toShow.Value);
+        }
+
+        public void Visualize() => visualizer.Visualize();
+    }
+}
