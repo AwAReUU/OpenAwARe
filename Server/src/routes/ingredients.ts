19c29c08c4332d7eea6bed7a8d1d62737ff3cc74
Nothoniel <nathanvdkolk@gmail.com>
2023-10-23 18:59:11 +0200

    updated SQL in auth and ingr routes

--- Server/src/routes/ingredients.ts
+++ Server/src/routes/ingredients.ts
@@ -42,48 +42,46 @@ router.get("/search", async (req: any, res: any) => {
 //
 // # Body
 // {
-//      id:          int,
-//      type:        string
+//      id:          int
 // }
 router.get("/getIngredient", async (req: any, res: any) => {
   const id = req.body.id;
-  const type = req.body.type;
 
   let db = Database.getInstance().db();
 
-  // we need to query different tables depending on the type of ingredient
-  // at the moment the type is specified in the request, we could also query Ingredient table to get this
-  // quite a bit of code duplication rn, could maybe just set the query string instead and (if not null) run db.get after...
-  switch (type) {
-    case "fruit":
-      db.get(
-        "SELECT * FROM TABLE Fruit WHERE IngredientID = ?",
-        [id],
-        async (error: any, row: any) => {
-          if (error) {
-            console.error(error);
-          }
-          res.status(200).json(row);
-        },
-      );
-      break;
-    case "animal":
-      db.get(
-        "SELECT * FROM TABLE Animal WHERE IngredientID = ?",
-        [id],
-        async (error: any, row: any) => {
-          if (error) {
-            console.error(error);
-          }
-          res.status(200).json(row);
-        },
-      );
-      break;
-    default:
-      // type does not exist
-      res.status(400).send("Invalid type.");
-      break;
-  }
+  db.get(
+    "SELECT * FROM TABLE Ingredient WHERE IngredientID = ?",
+    [id],
+    async (error: any, row: any) => {
+      if (error) {
+        console.error(error);
+      }
+      res.status(200).json(row);
+    },
+  );
+});
+
+// Get IngredientList
+//
+// # Body
+// {
+//      id:          int
+// }
+router.get("/getIngredientList", async (req: any, res: any) => {
+  const id = req.body.id;
+
+  let db = Database.getInstance().db();
+
+  db.all(
+    "SELECT * FROM TABLE Ingredient WHERE IngredientID = ?",
+    [id],
+    async (error: any, rows: any) => {
+      if (error) {
+        console.error(error);
+      }
+      res.status(200).json(rows);
+    },
+  );
 });
 
 export default router;
