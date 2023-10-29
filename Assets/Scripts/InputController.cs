8e2de2891a898153b74a5ea26a8ddf48d1cd819d
Mackthis <s.a.m.buitenhuis@students.uu.nl>
2023-10-29 17:16:26 +0100

    UI(Input)-refactor(InputHandlers): UNTESTED Splitting input-handlers.
    Untested refactor of input handlers. Applied strategy-like pattern to allow different responses to input dependent on later defined conditions (like Scene or InputState)
    
    Preparation for displaying VoxelMap, which has been broken in previous commits.

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,3 +1,5 @@
+using AwARe.DataTypes;
+using Newtonsoft.Json.Serialization;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,130 +8,298 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
 using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.UIElements;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
 
-public enum InputStates { All, Place, Interact }
-[RequireComponent(typeof(ARRaycastManager), typeof(ARPlaneManager))]
-public class InputController : MonoBehaviour
+namespace AwARe.InputControl
 {
-    private ARRaycastManager aRRaycastManager;
-    private ARPlaneManager aRPlaneManager;
-    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
-    private ObjectCreationManager objectCreationManager;
-    private InteractObjectHandler interactObjectHandler;
-    private Camera arCamera;
-    public InputStates inputState { get; private set; }
+    public enum InputState { All, Place, Interact }
 
-    public void SetInputState(InputStates state)
+    public class Ref<T>
     {
-        inputState = state;
-    }
-    
-    public void SetInputState(int index)
-    {
-        inputState = (InputStates)index;
+        public Ref(T value) =>
+            Value = value;
+
+        public T Value { get; set; }
+
+        public static implicit operator T(Ref<T> r) => r.Value;
     }
 
-    private void Awake()
+    public class Get<T>
     {
-        aRRaycastManager = GetComponent<ARRaycastManager>();
-        aRPlaneManager = GetComponent<ARPlaneManager>();
-        objectCreationManager = GetComponent<ObjectCreationManager>();
-        interactObjectHandler = GetComponent<InteractObjectHandler>();
-        arCamera = FindObjectOfType<Camera>();
-        SetInputState(InputStates.All);
+        public Get(System.Func<T> getter) =>
+            this.Getter = getter;
+
+        public System.Func<T> Getter { get; set; }
+
+        public T Value { get => Getter(); }
+
+        public static implicit operator Get<T>(System.Func<T> a) => new(a);
+        public static implicit operator T(Get<T> g) => g.Value;
     }
 
-    private void OnEnable()
+    internal interface IInputHandler
     {
-        EnhancedTouch.TouchSimulation.Enable();
-        EnhancedTouch.EnhancedTouchSupport.Enable();
-        //EnhancedTouch.Touch.onFingerDown += FingerDown;
+        public bool HandleInput(Vector2 screenPoint);
     }
 
-    private void OnDisable()
+    [RequireComponent(typeof(ARRaycastManager), typeof(ARPlaneManager))]
+    public class InputController : MonoBehaviour, IInputHandler
     {
-        //EnhancedTouch.Touch.onFingerDown -= FingerDown;
-        EnhancedTouch.TouchSimulation.Disable();
-        EnhancedTouch.EnhancedTouchSupport.Disable();
-    }
+        public InputState inputState;
 
-    //? I disabled all Fingerdown references as they are bugged, see IsPointerOverUIObject(). 
-    //? Inputs are detected through mouse in Update() which for some reason also detects finger inputs.
-    //* Interact using touch screen (mobile)
-    private void FingerDown(EnhancedTouch.Finger finger)
-    {
-        if (finger.index != 0) return;
+        private IInputHandler inputHandler;
 
-        HandleInput(finger.currentTouch.screenPosition);
-    }
+        private Vector2? firstFingerPosition;
+        private Vector2? mousePosition;
 
-    //* Interact using mouse button (pc)
-    //? Update should only be enabled within the editor, but it's currently enabled for mobile as well
-    //? Due to the bug explained at FingerDown() and IsPointerOverUIObject()
-    //#if UNITY_EDITOR
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-            HandleInput(Input.mousePosition);
-    }
-    //#endif
+        public void SetInputState(InputState state) =>
+            inputState = state;
 
-    private bool IsPointerOverUIObject()
-    {
-        //check mouse
-        if (EventSystem.current.IsPointerOverGameObject())
-            return true;
+        public void SetInputState(int index) =>
+            inputState = (InputState)index;
 
-        //check touch
-        //TODO Fix this input bug
-        //! This is bugged; it's like a touch delayed. 
-        //! E.g. when I press a UI button the first time it still sends a raycast behind the button. 
-        //! After the first touch it works correctly, but then the first (and only the first) touch
-        //! in the worldscene is not detected, after which world inputs work correctly again.
-        // if (EnhancedTouch.Touch.activeTouches.Count > 0)
-        // {
-        //     if (EventSystem.current.IsPointerOverGameObject(EnhancedTouch.Touch.activeTouches[0].touchId))
-        //     {
-        //         return true;
-        //     }
-        // }
-
-        return false;
-    }
-
-    private void HandleInput(Vector2 screenPoint)
-    {
-        if (IsPointerOverUIObject())
-            return;
-
-        //* Interact with existing object in world
-        if (inputState == InputStates.Interact || inputState == InputStates.All)
+        private void Awake()
         {
+            inputState = InputState.All;
+
+            var aRRaycastManager = GetComponent<ARRaycastManager>();
+            var aRPlaneManager = GetComponent<ARPlaneManager>();
+            var objectCreationManager = GetComponent<ObjectCreationManager>();
+            var interactObjectHandler = GetComponent<InteractObjectHandler>();
+            var arCamera = FindObjectOfType<Camera>();
+            var getInputState = (Get<InputState>)(() => inputState);
+
+            InputHandlerFactory factory = new InputHandlerFactory();
+            inputHandler = factory.CreateRegular(arCamera, interactObjectHandler, aRRaycastManager, objectCreationManager, getInputState);
+        }
+
+        private void OnEnable()
+        {
+            EnhancedTouch.TouchSimulation.Enable();
+            EnhancedTouch.EnhancedTouchSupport.Enable();
+            //EnhancedTouch.Touch.onFingerDown += FingerDown;
+        }
+
+        private void OnDisable()
+        {
+            //EnhancedTouch.Touch.onFingerDown -= FingerDown;
+            EnhancedTouch.TouchSimulation.Disable();
+            EnhancedTouch.EnhancedTouchSupport.Disable();
+        }
+
+        //? I disabled all Fingerdown references as they are bugged, see IsPointerOverUIObject(). 
+        //? Inputs are detected through mouse in Update() which for some reason also detects finger inputs.
+        //* Interact using touch screen (mobile)
+        private void FingerDown(EnhancedTouch.Finger finger)
+        {
+            if (finger.index == 0) 
+                firstFingerPosition = finger.currentTouch.screenPosition;
+        }
+
+        private void MouseButtonDown() =>
+            mousePosition = Input.GetMouseButtonDown(0) ? Input.mousePosition : null;
+
+        //* Interact using mouse button (pc)
+        //? Update should only be enabled within the editor, but it's currently enabled for mobile as well
+        //? Due to the bug explained at FingerDown() and IsPointerOverUIObject()
+        //#if UNITY_EDITOR
+        private void Update()
+        {
+            // Update input
+            MouseButtonDown();
+
+            // Check input
+            Vector2? screenPosition = firstFingerPosition ?? mousePosition;
+            if (screenPosition.HasValue) HandleInput(screenPosition.Value);
+        }
+        //#endif
+
+        public bool HandleInput(Vector2 screenPoint) =>
+            inputHandler.HandleInput(screenPoint);
+    }
+
+    public class InputHandler_Regular : IInputHandler
+    {
+        private InputHandler_PressUI presser;
+        private InputHandler_SelectObject selecter;
+        private InputHandler_PlaceObject placer;
+        private Get<InputState> inputState;
+
+        public InputHandler_Regular(InputHandler_PressUI presser, InputHandler_SelectObject selecter, InputHandler_PlaceObject placer, Get<InputState> inputState)
+        {
+            this.presser = presser; this.selecter = selecter; this.placer = placer; this.inputState = inputState;
+        }
+
+        public bool HandleInput(Vector2 screenPoint)
+        {
+            //* Interact with existing UI in world
+            if (presser.HandleInput(screenPoint)) return true;
+
+            //* Interact with existing object in world
+            if ((inputState == InputState.Interact || inputState == InputState.All))
+                if (selecter.HandleInput(screenPoint)) return true;
+
+            //* Place new object in world
+            if (inputState == InputState.Place || inputState == InputState.All)
+                if (placer.HandleInput(screenPoint)) return true;
+
+            return false;
+        }
+    }
+
+    public class InputHandler_VoxelMap : IInputHandler
+    {
+        private InputHandler_PressUI presser;
+
+        public InputHandler_VoxelMap(InputHandler_PressUI presser)
+        {
+            this.presser = presser;
+        }
+
+        public bool HandleInput(Vector2 screenPoint)
+        {
+            //* Interact with existing UI in world
+            if (presser.HandleInput(screenPoint)) return true;
+
+            return false;
+        }
+    }
+
+    public class InputHandler_SelectObject : IInputHandler
+    {
+        private Camera arCamera;
+        private InteractObjectHandler interactObjectHandler;
+
+        public InputHandler_SelectObject(Camera arCamera, InteractObjectHandler interactObjectHandler)
+        {
+            this.arCamera = arCamera; this.interactObjectHandler = interactObjectHandler;
+        }
+
+        public bool HandleInput(Vector2 screenPoint)
+        {
+            //* Interact with existing object in world
             Ray ray = arCamera.ScreenPointToRay(screenPoint);
-            RaycastHit hitObject;
-            if (Physics.Raycast(ray, out hitObject, 20f, LayerMask.GetMask("Ingredient")))
+            if (Physics.Raycast(ray, out RaycastHit hitObject, 20f, LayerMask.GetMask("Ingredient")))
             {
                 //interactObjectHandler.ColorObject(hitObject.transform.gameObject);
                 interactObjectHandler.ToggleDataWindow(hitObject.transform.gameObject);
-                return;
+                return true;
             }
+            return false;
+        }
+    }
+
+    public class InputHandler_PlaceObject : IInputHandler
+    {
+        private ARRaycastManager aRRaycastManager;
+        private ObjectCreationManager objectCreationManager;
+
+        private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+        public InputHandler_PlaceObject(ARRaycastManager aRRaycastManager, ObjectCreationManager objectCreationManager)
+        {
+            this.aRRaycastManager = aRRaycastManager; this.objectCreationManager = objectCreationManager;
         }
 
-        //* Place new object in world
-        if (inputState == InputStates.Place || inputState == InputStates.All)
+        public bool HandleInput(Vector2 screenPoint)
         {
+            //* Place new object in world
             if (aRRaycastManager.Raycast(screenPoint, hits, TrackableType.PlaneWithinPolygon))
             {
                 // foreach (ARRaycastHit hit in hits)
                 //     createObjectHandler.CreateObject(hit);
                 if (hits.Count > 0)
+                {
                     objectCreationManager.TryPlaceObjectOnTouch(hits[0]);
+                    return true;
+                }
             }
+            return false;
         }
+    }
+
+    public class InputHandler_PressUI : IInputHandler
+    {
+        public InputHandler_PressUI() { }
+
+        private bool IsPointerOverUIObject()
+        {
+            //check mouse
+            if (EventSystem.current.IsPointerOverGameObject())
+                return true;
+
+            //check touch
+            //TODO Fix this input bug
+            //! This is bugged; it's like a touch delayed. 
+            //! E.g. when I press a UI button the first time it still sends a raycast behind the button. 
+            //! After the first touch it works correctly, but then the first (and only the first) touch
+            //! in the worldscene is not detected, after which world inputs work correctly again.
+            // if (EnhancedTouch.Touch.activeTouches.Count > 0)
+            // {
+            //     if (EventSystem.current.IsPointerOverGameObject(EnhancedTouch.Touch.activeTouches[0].touchId))
+            //     {
+            //         return true;
+            //     }
+            // }
+
+            return false;
+        }
+
+        public bool HandleInput(Vector2 screenPoint) =>
+            IsPointerOverUIObject();
+    }
+
+    public class InputHandlerFactory
+    {
+        public InputHandlerFactory() { }
+
+        public InputHandler_PressUI CreatePressUI() =>
+            new();
+
+        public InputHandler_SelectObject CreateSelectObject(Camera arCamera, InteractObjectHandler interactObjectHandler) =>
+            new(arCamera, interactObjectHandler);
+
+        public InputHandler_PlaceObject CreatePlaceObject(ARRaycastManager aRRaycastManager, ObjectCreationManager objectCreationManager) =>
+            new(aRRaycastManager, objectCreationManager);
+
+
+        public InputHandler_Regular CreateRegular(Camera arCamera, InteractObjectHandler interactObjectHandler, ARRaycastManager aRRaycastManager, ObjectCreationManager objectCreationManager, Get<InputState> inputState) =>
+            new(CreatePressUI(), CreateSelectObject(arCamera, interactObjectHandler), CreatePlaceObject(aRRaycastManager, objectCreationManager), inputState);
+
+        public InputHandler_VoxelMap CreateVoxelMap() =>
+            new(new());
+
+        public InputHandlers CreateInputHandlers(Camera arCamera, InteractObjectHandler interactObjectHandler, ARRaycastManager aRRaycastManager, ObjectCreationManager objectCreationManager, Get<InputState> inputState)
+        {
+            var pressUI = new InputHandler_PressUI();
+            var selectObject = new InputHandler_SelectObject(arCamera, interactObjectHandler);
+            var placeObject = new InputHandler_PlaceObject(aRRaycastManager, objectCreationManager);
+            var regular = new InputHandler_Regular(pressUI, selectObject, placeObject, inputState);
+            var voxelmap = new InputHandler_VoxelMap(pressUI);
+            return new(pressUI, selectObject, placeObject, regular, voxelmap);
+        }
+    }
+
+    public class InputHandlers
+    {
+        public InputHandlers(
+            InputHandler_PressUI pressUI, InputHandler_SelectObject selectObject, InputHandler_PlaceObject placeObject,
+            InputHandler_Regular regular, InputHandler_VoxelMap voxelmap
+            )
+        {
+            PressUI = pressUI; SelectObject = selectObject; PlaceObject = placeObject;
+            Regular = regular; VoxelMap = voxelmap;
+        }
+
+        public InputHandler_PressUI PressUI { get; private set; }
+        public InputHandler_SelectObject SelectObject { get; private set; }
+        public InputHandler_PlaceObject PlaceObject { get; private set; }
+        public InputHandler_Regular Regular { get; private set; }
+        public InputHandler_VoxelMap VoxelMap { get; private set; }
     }
 }
