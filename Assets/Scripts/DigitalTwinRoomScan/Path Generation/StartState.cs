eebfdd3c74a042104a205af731e73b97654d19ca
Marco <m.r.kamphuis@students.uu.nl>
2023-11-09 14:15:31 +0100

    N23D-441-DigTwin(Path Gen)-feat-startstate
    Implemented the first (still work in progress and incorrect) version of the startstate for the pathfinding algorithm

---
+++ Assets/Scripts/DigitalTwinRoomScan/Path Generation/StartState.cs
@@ -0,0 +1,296 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class StartState 
+{
+    public PathData GetStartState(Polygon positive, List<Polygon> negatives)
+    {
+        PathData result = new();
+
+
+        //shoot a ray from every corner point in the positive mesh
+        //shoot a ray from every corner point in the negative mesh in the opposite direction
+        //ray direction = make the 2 lines of the point into a parallelogram by virtually copying
+
+        List<(Vector3, Vector3)> positiveLines = GenerateLines(positive);
+        List<(Vector3, Vector3)> allRays = generateRays(positiveLines);
+
+        for (int i = 0; i < negatives.Count; i++)
+        {
+            List<(Vector3, Vector3)> negativeLines = GenerateLines(negatives[i]);
+            List<(Vector3, Vector3)> negativeRays = generateRays(negativeLines, true);
+
+            for(int j = 0; j < negativeRays.Count; j++)
+            {
+                allRays.Add(negativeRays[i]);
+            }
+        }
+
+        List<Vector3> allIntersections = new();
+
+        //find all ray intersections by checking each ray against each other ray
+        for(int i = 0; i < allRays.Count - 1; i++)
+        {
+            for (int j = i + 1; j < allRays.Count; j++)
+            {
+                bool intersects;
+                Vector3 point = IntersectionPoint(allRays[i].Item1, allRays[i].Item2, allRays[j].Item1, allRays[j].Item2, out intersects, 3);
+                
+                if(!intersects)
+                {
+                    continue;
+                }
+
+                //check if the point is in the polygon
+                if(CheckInPolygon(positive, point))
+                {
+                    allIntersections.Add(point);
+                }
+            }
+        }
+
+        //potential issue: order of the points in the list is probably not correct
+        //i am still looking into this
+        result.points = allIntersections;
+        result.radius = 1;      //arbitrarily chosen number, feel free to alter to something better
+
+        return result;
+    }
+
+    /// <summary>
+    /// Calculate the intersection point of 2 lines or rays
+    /// </summary>
+    /// <param name="l1p1">line 1 point 1</param>
+    /// <param name="l1p2">line 1 point 2</param>
+    /// <param name="l2p1">line 2 point 1</param>
+    /// <param name="l2p2">line 2 point 2</param>
+    /// <param name="intersects"> returned bool that determines if the lines intersect. </param>
+    /// <param name="mode"> the mode of the method, defaults to 0.
+    /// in mode 0, both lines provided are treated as finite lines in both directions
+    /// in mode 1, line 1 is treated as finite in both directions but line 2 is treated as infinite in both directions
+    /// in mode 2, line 1 is infinite in the direction of point 2, but finite in the direction of point 1. line 1 is finite in both directions
+    /// in mode 3, both lines are  infinite in the direction of point 2, but finite in the direction of point 1.
+    /// in mode 4, both lines are  infinite in both directions.
+    /// </param>
+    /// <returns> point of intersection </returns>
+    private Vector3 IntersectionPoint(Vector3 l1p1, Vector3 l1p2, Vector3 l2p1, Vector3 l2p2, out bool intersects, byte mode = 0)
+    {
+        double dx1 = l1p2.x - l1p1.x;
+        double dy1 = l1p2.y - l1p1.y;
+        double dx2 = l2p2.x - l2p1.x;
+        double dy2 = l2p2.y - l2p1.y;
+
+        double divider = dx2 * dy1 - dy2 * dx1;
+
+        if (divider == 0)
+        {
+            //lines are parallel and thus don't intersect
+            intersects = false;
+            return Vector3.zero;
+        }
+
+        double t1 = ((l1p1.x - l2p1.x) * dy2 + (l2p1.y - l1p1.y) * dx2) / divider;
+        double t2 = ((l1p1.x - l2p1.x) * dy1 + (l2p1.y - l1p1.y) * dx1) / divider;
+
+        //potential bug: intinite / finite in the wrong direction
+
+        switch (mode)
+        {
+            //both lines are finite in both directions
+            case 0:
+                if(!(t1 >= 0 && t1 <= 1 && t2 >= 0 && t2 <= 1))
+                {
+                    intersects = false;
+                    return Vector3.zero;
+                }
+                break;
+            //line 1 is finite in both directions. line 2 is infinite in both directions
+            case 1:
+                if (!(t1 >= 0 && t1 <= 1))
+                {
+                    intersects = false;
+                    return Vector3.zero;
+                }
+                break;
+            //line 1 is infinite only in the direction of point 2. line 2 is finite in foth directions
+            case 2:
+                if (!(t1 >= 0 && t2 >= 0 && t2 <= 1))
+                {
+                    intersects = false;
+                    return Vector3.zero;
+                }
+                break;
+            //both lines are infinite only in the direction of point 2.
+            case 3:
+                if (!(t1 >= 0 && t2 >= 0))
+                {
+                    intersects = false;
+                    return Vector3.zero;
+                }
+                break;
+            //both lines are infinite in both directions
+            case 4:
+                break;
+        }
+
+        intersects = true;
+        float intersectx = (float)(l1p1.x + t1 * dx1);
+        float intersecty = (float)(l1p1.y + t1 * dy1);
+        Vector3 intersectionPoint = new Vector3(intersectx, intersecty);
+        return intersectionPoint;
+    }
+
+    /// <summary>
+    /// turns the polygon into a list of its line segments signified by start and endpoint
+    /// </summary>
+    /// <param name="polygon">the polygon from which to create the list</param>
+    /// <returns>list of line segments, signified by 2 points</returns>
+    
+    //potential (unit) test: check if the second item of every point is equal to the first item of the next point in the list
+    private List<(Vector3, Vector3)> GenerateLines(Polygon polygon)
+    {
+        List<(Vector3, Vector3)> results = new();
+        List<Vector3> points = polygon.GetPoints().ToList();
+        //add a duplicate of the first point to the end of the list
+        points.Add(new Vector3(points[0].x, points[0].y));
+
+        for(int i = 0; i < points.Count -1; i++)
+        {
+            results.Add((points[i], points[i + 1]));
+        }
+
+        return results;
+    }
+
+    /// <summary>
+    /// generate a list of rays based on a given list of line segments
+    /// it is assumed each segment in the list is adjacent to the previous segment,
+    /// and that the first segment is adjacent to the last
+    /// </summary>
+    /// <param name="lines">list of line segments</param>
+    /// <param name="mode">if false, generate rays for a 'positive' polygon. Otherwise generate them for a 'negative' polygon</param>
+    /// <returns>list of rays signified by 2 points. fired from point 1 in the direction of point 2.</returns>
+    private List<(Vector3, Vector3)> generateRays(List<(Vector3, Vector3)> lines, bool mode = false)
+    {
+        List<(Vector3, Vector3)> rays = new();
+
+        //generate all the rays
+        //check the direction of the rays by comparing with line intersect in the correct mode
+        //any who are in the incorrect direction (as for a positive) are flipped
+
+        //add a copy of the first line segment to the end of the list
+        lines.Add((new Vector3(lines[0].Item1.x, lines[0].Item1.y), new Vector3(lines[0].Item2.x, lines[0].Item2.y)));
+
+        //generate all the rays
+        for(int i = 0; i < lines.Count - 1; i++)
+        {
+            Vector3 rayStart = lines[i].Item1;
+            Vector3 rayEnd = lines[i + 1].Item2 + (lines[i].Item1 - lines[i].Item2);
+            rays.Add((rayStart, rayEnd));
+        }
+
+        //remove the copied line
+        lines.RemoveAt(lines.Count - 1);
+        //check if the direction of the ray is correct
+
+        //warning for potential bug: if the #intersections is still even after flipping, then the ray intersects with its own origin point
+        //if it does this, potential fixes are: slighlty move the origin point, or alter the intersection methods t1 and t2 checks to be 
+        //to be '>' and '<' instead of '>=' and '<='.
+        //i do not believe this can happen because the origin point should then intersect 2 polygon edges, thus always adding 2 to the number
+        //of intersections, and so even will remain even and odd will remain odd, not influencing the outcome.
+
+
+        for(int i = 0; i < rays.Count; i++)
+        {
+            int intersections = 0;
+
+            for(int j = 0; j < lines.Count; j++)
+            {
+                bool intersects;
+                //use mode 2 as we shoot a ray out of a point, looking for intersections with finite line segments
+                var _ = IntersectionPoint(rays[i].Item1, rays[i].Item2, lines[j].Item1, lines[j].Item2, out intersects, 2);
+                if (intersects) intersections++;
+            }
+            //if the number of intersections is even, it is going out of the polygon, and we need to flip the direction
+            if(intersections % 2 == 0)
+            {
+                rays[i] = FlipRay(rays[i]);
+            }
+        }
+
+        if(mode)
+        {
+            for (int i = 0; i < rays.Count; i++)
+            {
+                rays[i] = FlipRay(rays[i]);
+            }
+        }
+
+        return rays;
+    }
+
+    /// <summary>
+    /// flips a given ray, effectively spins it 180 degrees around its origin point.
+    /// </summary>
+    /// <param name="ray"> the ray to be flipped </param>
+    /// <returns> the flipped ray </returns>
+    private (Vector3, Vector3) FlipRay((Vector3, Vector3) ray)
+    {
+        Vector3 origin = ray.Item1;
+        Vector3 destination = ray.Item1 - (ray.Item2 - ray.Item1);
+
+        return (origin, destination);
+    }
+
+    private bool CheckInPolygon(Polygon polygon, Vector3 point)
+    {
+
+        //ideas:
+        //shoot ray towards a random point in the polygon         //problem: point is adjacent to 2 edges
+        //shoot ray towards average center point in the polygon   //problem: could still pass through a perfect edge point with low probability
+        //do the second one
+
+        List<(Vector3, Vector3)> lines = GenerateLines(polygon);
+
+        Vector3 centerPolygonPoint = Vector3.zero;
+        float averageFactor = 1 / polygon.GetPoints().Length;
+        for(int i = 0; i < polygon.GetPoints().Length; i++)
+        {
+            centerPolygonPoint = centerPolygonPoint + (polygon.GetPoints()[i] * averageFactor);
+        }
+
+        (Vector3, Vector3) ray = (point, centerPolygonPoint);
+
+        int intersections = 0;
+        for (int j = 0; j < lines.Count; j++)
+        {
+            bool intersects;
+            //use mode 2 as we shoot a ray out of a point, looking for intersections with finite line segments
+            var _ = IntersectionPoint(ray.Item1, ray.Item2, lines[j].Item1, lines[j].Item2, out intersects, 2);
+            if (intersects) intersections++;
+        }
+
+        //if the number of intersections is even, the point is outside of the polygon
+        if (intersections % 2 == 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    //genlines method voor de polygons. 1 voor beide tegelijk is goed                                    done
+    //genrays method voor de polygons. pos en neg een apparte, omdat neg rays de andere kant op moeten.  done (alternatively)
+    //alternatively, 1 voor beide, en dan een method om pos rays in neg rays te veranderen               done
+    //method om ray intersections te calculaten en de intersection points te returnen                    done
+    //method om een path te maken tussen deze intersection points                                        done but probably not good enough
+    //maybe een method om nearby intersections te refinen door clusteren                                 optional todo
+    //maybe een method om de 'roundness' van de polygon te detecten en de polygon te refinen door wat cornerpoints te mergen    optional todo
+}
