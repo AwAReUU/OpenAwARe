5ed234818fd373bdfa832ebf81a17703fb5623d4
U32Float <123883732+U32Float@users.noreply.github.com>
2023-11-28 15:43:58 +0100

    Update ingr.test.ts

--- Server/src/__tests__/ingr.test.ts
+++ Server/src/__tests__/ingr.test.ts
@@ -3,17 +3,65 @@ import { get_authorization_header } from "./util";
 
 // ----------------------------------------------------------------------------
 
-describe("Test all protected routes in /ingr/ without an authorization header. \
-          Each route should return a 400 error", () => {
+describe("Test if routes are protected.", () => {
   test("GET /ingr/search", async () => {
     let body = {
       query: "apple",
     };
     await api.get("/ingr/search").send(body).expect(400);
   });
+
+  test("GET /ingr/getIngredient", async () => {
+    let body = {
+      id: 0,
+    };
+    await api.get("/ingr/getIngredient").send(body).expect(400);
+  });
+
+  test("GET /ingr/getIngredientList", async () => {
+    let body = {
+      ids: [0, 1, 2],
+    };
+    await api.get("/ingr/getIngredientList").send(body).expect(400);
+  });
+
+  test("GET /ingr/getRequirements", async () => {
+    let body = {
+      ids: 0,
+    };
+    await api.get("/ingr/getRequirements").send(body).expect(400);
+  });
+
+  test("GET /ingr/getResource", async () => {
+    let body = {
+      ids: 0,
+    };
+    await api.get("/ingr/getResource").send(body).expect(400);
+  });
+
+  test("GET /ingr/getResourceList", async () => {
+    let body = {
+      ids: [0, 1, 2],
+    };
+    await api.get("/ingr/getResourceList").send(body).expect(400);
+  });
+
+  test("GET /ingr/getModel", async () => {
+    let body = {
+      ids: 0,
+    };
+    await api.get("/ingr/getModel").send(body).expect(400);
+  });
+
+  test("GET /ingr/getModelList", async () => {
+    let body = {
+      ids: [0, 1, 2],
+    };
+    await api.get("/ingr/getModelList").send(body).expect(400);
+  });
 });
 
-describe("Test all protected routes in /ingr/ with an active login session.", () => {
+describe("Test if routes are working properly", () => {
   test("GET /ingr/search", async () => {
     let body = {
       query: "apple",
