8bbb4d2dc7caed93225118bccb8842db1725ddf1
U32Float <123883732+U32Float@users.noreply.github.com>
2023-12-08 14:11:16 +0100

    Add comments

--- AwARe/Assets/FirstParty/Application/Scripts/MainFeatures/RoomScan/Path/VisualizePath.cs
+++ AwARe/Assets/FirstParty/Application/Scripts/MainFeatures/RoomScan/Path/VisualizePath.cs
@@ -6,19 +6,40 @@ namespace AwARe.RoomScan.Path
     public class VisualizePath : MonoBehaviour
     {
         [Header("Path")]
+        /// <value>
+        /// The data of the path to visualize
+        /// </value>
         [SerializeField] private PathData pathData;
 
         [Header("Settings")]
+        /// <value>
+        /// The width of the line that visualizes the skeleton of the path.
+        /// </value>
         [SerializeField] private float lineWidth = 1.0f;
+        /// <value>
+        /// The number of segments to use for the corners. Use a higher value to create smoother edges.
+        /// </value>
         [SerializeField] private int numSegments = 6;
+        /// <value>
+        /// The material used to render the path Mesh.
+        /// </value>
         [SerializeField] private Material pathMeshMaterial;
+        /// <value>
+        /// The material used to render the skeleton line.
+        /// </value>
         [SerializeField] private Material pathLineMaterial;
 
+        /// <summary>
+        /// Set the path to visualize.
+        /// </summary>
         public void SetPath(PathData path)
         {
             this.pathData = path;
         }
 
+        /// <summary>
+        /// Visualize the path. It will clear previous visualisations.
+        /// </summary>
         public void Visualize()
         {
             // Clear previous visualisation
