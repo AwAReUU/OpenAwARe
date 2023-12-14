99455669e765c8c118bd21ade499a973e34501e6
Eurahh <kiranahuijs@gmail.com>
2023-12-14 15:17:17 +0100

    N23D-568-General/Doc-Comments-feat: added UI comments

--- AwARe/Assets/FirstParty/Application/Scripts/General/UI/Objects/PopUps/PopUp.cs
+++ AwARe/Assets/FirstParty/Application/Scripts/General/UI/Objects/PopUps/PopUp.cs
@@ -8,14 +8,21 @@ using UnityEngine;
 
 namespace AwARe.UI.Objects
 {
+    /// <summary>
+    /// Controls the activation state of a pop-up panel in the graphical user interface.
+    /// </summary>
     public class PopUpScript : MonoBehaviour
     {
         [SerializeField] private GameObject popupPanel;
 
-        // popUp is set to active or inactive
+        /// <summary>
+        /// Sets the pop-up panel to an active state, making it visible.
+        /// </summary>
         public void PopUpOn() =>
             popupPanel.SetActive(true);
-
+        /// <summary>
+        /// Sets the pop-up panel to an inactive state, hiding it from view.
+        /// </summary>
         public void PopUpOff() =>
             popupPanel.SetActive(false);
     }
