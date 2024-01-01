1f8087c994cc1d10d916388121d672d6677b2a2b
Eurahh <kiranahuijs@gmail.com>
2024-01-01 21:34:56 +0100

    Account-UI feat(register and login page): added all the components of register and login page in account ui scene

---
+++ AwARe/Assets/FirstParty/Application/Scripts/General/UI/Objects/InputFields/AccountUIInputFields.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AwARe
+{
+    public class AccountUIInputFields : MonoBehaviour
+    {
+
+        public InputField loginEmailInputField;
+        public InputField loginPasswordInputField;
+
+        public InputField registerEmailInputField;
+        public InputField registerPasswordInputField;
+        public InputField FirstNameInputField;
+        public InputField LastNameInputField;
+        public InputField passwordConfirmInputField;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
