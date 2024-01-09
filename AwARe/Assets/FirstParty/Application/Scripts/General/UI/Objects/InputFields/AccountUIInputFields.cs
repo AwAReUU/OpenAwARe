9701a85a537b86f6b2545cfb5b5dff64bd25f757
Eurahh <kiranahuijs@gmail.com>
2024-01-09 14:20:32 +0100

    Account-UI feat(errorhandeling submission): UNTESTED added code for control strong password, added code to check if all fields are filled and added code for user to see password

--- AwARe/Assets/FirstParty/Application/Scripts/General/UI/Objects/InputFields/AccountUIInputFields.cs
+++ AwARe/Assets/FirstParty/Application/Scripts/General/UI/Objects/InputFields/AccountUIInputFields.cs
@@ -1,5 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+using TMPro;
+
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,40 +12,75 @@ namespace AwARe
     public class AccountUIInputFields : MonoBehaviour
     {
         // input fields
-        public InputField loginEmailInputField;
-        public InputField loginPasswordInputField;
-        public InputField registerEmailInputField;
-        public InputField registerPasswordInputField;
-        public InputField FirstNameInputField;
-        public InputField LastNameInputField;
-        public InputField passwordConfirmInputField;
+        [SerializeField] private TMP_InputField loginEmailInputField;
+        [SerializeField] private TMP_InputField loginPasswordInputField;
+        [SerializeField] private TMP_InputField registerEmailInputField;
+        [SerializeField] private TMP_InputField registerPasswordInputField;
+        [SerializeField] private TMP_InputField FirstNameInputField;
+        [SerializeField] private TMP_InputField LastNameInputField;
+        [SerializeField] private TMP_InputField passwordConfirmInputField;
+        [SerializeField] private Button securityButton;
 
-        // 
+
+        private bool IsStrongPassword(string password)
+        {
+            // Password should be at least 12 characters long
+            if (password.Length < 12)
+            {
+                return false;
+            }
+
+            // Password should have a combination of uppercase letters, lowercase letters, numbers, and symbols
+            Regex uppercaseRegex = new Regex(@"[A-Z]");
+            Regex lowercaseRegex = new Regex(@"[a-z]");
+            Regex digitRegex = new Regex(@"\d");
+            Regex symbolRegex = new Regex(@"[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]");
+
+            return uppercaseRegex.IsMatch(password) && lowercaseRegex.IsMatch(password) &&
+                digitRegex.IsMatch(password) && symbolRegex.IsMatch(password);
+        }
 
 
         // Start is called before the first frame update
         void Start()
         {
-            string registeremail = registerEmailInputField.text;
-            string registerpassword = registerPasswordInputField.text;
-            string registerfirstname = FirstNameInputField.text;
-            string registerlastname = LastNameInputField.text;
-            string registerconfirmpassword = passwordConfirmInputField.text;
 
+           
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            securityButton.onClick.AddListener(delegate () { this.OnSecurityButtonClick(); });
+
         }
         public void OnLoginButtonClick()
         {
   
         }
 
+        public void OnSecurityButtonClick()
+        {
+            registerPasswordInputField.contentType = TMP_InputField.ContentType.Standard;
+
+        }
+
         public void OnRegisterButtonClick()
         {
+
+            string registeremail = registerEmailInputField.text;
+            string registerpassword = registerPasswordInputField.text;
+            string registerfirstname = FirstNameInputField.text;
+            string registerlastname = LastNameInputField.text;
+            string registerconfirmpassword = passwordConfirmInputField.text;
+
+            if (string.IsNullOrEmpty(registeremail) || string.IsNullOrEmpty(registerpassword) ||
+                string.IsNullOrEmpty(registerfirstname) || string.IsNullOrEmpty(registerlastname) ||
+                string.IsNullOrEmpty(registerconfirmpassword))
+            {
+                Debug.LogError("Please fill in all required fields.");
+                return; // Stop execution if any field is empty
+            }
 
         }
 
