6d3d91962e424ebbd5bcc7f8390a0e4b66c1ef86
Seana <73309041+Seana-O@users.noreply.github.com>
2024-01-09 18:23:41 +0100

    DigTwin(Anchorpoints)-feat: Displaying screenshots
    JIRA: N23D-585
    
    Summary:
    Showing screenshots on the screen.

--- AwARe/Assets/FirstParty/Application/Scripts/MainFeatures/RoomScan/ScreenshotManager.cs
+++ AwARe/Assets/FirstParty/Application/Scripts/MainFeatures/RoomScan/ScreenshotManager.cs
@@ -9,6 +9,8 @@ using System;
 using AwARe.RoomScan.Polygons.Logic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System.Collections;
 
 namespace AwARe.RoomScan
 {
@@ -17,29 +19,73 @@ namespace AwARe.RoomScan
     /// </summary>
     public class ScreenshotManager : MonoBehaviour
     {
-        Image image;
+        [SerializeField] private Canvas canvas;
+        [SerializeField] private Sprite maskSprite;
+        private Image image;
+        private GameObject maskObj;
 
-        private void Start()
+        bool displayed = false;
+        bool setup = true;
+
+        void Start()
         {
-            // create an image
-            image = gameObject.AddComponent<Image>();
+            SetupImage();
 
-            // make the image transparent
-            Color color = image.color;
-            color.a = 0.5f;
-            image.color = color;
+            maskObj.SetActive(false);
+
+            File.Delete(GetPath(0));
+        }
+        void Update()
+        {
+            if (!setup)
+            {
+                setup = true;
+                SetupImage();
+            }
+            if ((UnityEngine.Input.touchCount > 0 && UnityEngine.Input.GetTouch(0).phase == TouchPhase.Began)
+                || UnityEngine.Input.GetMouseButtonDown(0))
+            {
+                if(displayed)
+                {
+                    displayed = false;
+                    maskObj.SetActive(false);
+                    File.Delete(GetPath(0));
+                }
+                else
+                {
+                    StartCoroutine(TakeScreenshot(0));
+                    Debug.Log("screenshot");
+                }
+            }
+            else
+            {
+                if (System.IO.File.Exists(GetPath(0)) && !displayed)
+                {
+                    Debug.Log("display");
+                    DisplayScreenshot(0);
+                    displayed = true;
+                }
+            }
         }
 
-        private string GetPath(Room room, int index) => "" + index; // TODO: add room name
+        private string GetPath(/*Room room, */int index) => $"{Application.persistentDataPath}{index}.png"; // TODO: add room name
 
-        private void TakeScreenshot(Room room, int index)
+        private IEnumerator TakeScreenshot(/*Room room, */int index)
         {
-            ScreenCapture.CaptureScreenshot(GetPath(room, index)); 
+            yield return new WaitForEndOfFrame();
+
+
+            Texture2D screenshot = ScreenCapture.CaptureScreenshotAsTexture();
+            System.IO.File.WriteAllBytes(GetPath(/*room, */0), screenshot.EncodeToPNG());
+
+            // TODO: display text: screenshot taken
         }
 
-        private void DisplayScreenshot(Room room, int index)
+        private void DisplayScreenshot(/*Room room, */int index)
         {
-            image.sprite = RetrieveScreenshot(GetPath(room, index));
+            //image.gameObject.SetActive(true);
+            maskObj.SetActive(true);
+            image.sprite = RetrieveScreenshot(GetPath(/*room, */index));
         }
 
         private Sprite RetrieveScreenshot(string filePath)
@@ -54,6 +100,42 @@ namespace AwARe.RoomScan
             throw new Exception("Screenshot cannot be found.");
         }
 
-        private void ClearImage() => image.sprite = null;
+        private void ClearImage()
+        {
+            image.sprite = null;
+            //image.gameObject.SetActive(false);
+        }
+
+        private void SetupImage()
+        {
+            // create a mask object
+            maskObj = new GameObject("ScreenshotMask");
+            maskObj.transform.parent = canvas.transform;
+            Image maskImage = maskObj.AddComponent<Image>();
+            maskObj.AddComponent<Mask>();
+            maskImage.sprite = maskSprite;
+            maskImage.color = Color.black;
+
+            // create an image object
+            GameObject imageObj = new GameObject("ScreenshotImage");
+            imageObj.transform.parent = maskObj.transform;
+            image = imageObj.AddComponent<Image>();
+
+            // set image size to match screensize
+            //maskImage.rectTransform.pivot = new Vector2(0, 0);
+            float maskSize = Screen.width / 2;
+            maskImage.rectTransform.sizeDelta = new Vector2(maskSize, maskSize);
+            maskImage.rectTransform.localPosition = new Vector3(0, 0, 0);
+            maskImage.raycastTarget = false;
+
+            image.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+
+            // make the image transparent
+            Color color = image.color;
+            color.a = 0.5f;
+            image.color = color;
+
+            ClearImage();
+        }
     }
 }
