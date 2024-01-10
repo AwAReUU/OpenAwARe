68035458733032805a2ac0cef0c5fb227d8db8b0
Seana <73309041+Seana-O@users.noreply.github.com>
2024-01-10 14:57:50 +0100

    DigTwin(Anchorpoints)-refactor: ScreenshotManager
    JIRA: N23D-585
    
    Summary:
    Refactored and added docs to ScreenshotManager

--- AwARe/Assets/FirstParty/Application/Scripts/MainFeatures/RoomScan/ScreenshotManager.cs
+++ AwARe/Assets/FirstParty/Application/Scripts/MainFeatures/RoomScan/ScreenshotManager.cs
@@ -6,11 +6,11 @@
 // \*                                                                                       */
 
 using System;
-using AwARe.RoomScan.Polygons.Logic;
+using System.Collections;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
-using System.IO;
-using System.Collections;
+using AwARe.RoomScan.Polygons.Logic;
 
 namespace AwARe.RoomScan
 {
@@ -22,89 +22,100 @@ namespace AwARe.RoomScan
         [SerializeField] private Canvas canvas;
         [SerializeField] private Sprite maskSprite;
         [SerializeField] private Sprite outlineSprite;
+
         private Image image;
         private GameObject maskObj;
-
-        bool displayed = false;
-        bool setup = true;
 
         void Start()
         {
             SetupImage();
 
             maskObj.SetActive(false);
-
-            File.Delete(GetPath(0));
         }
+
         void Update()
         {
-            if (!setup)
-            {
-                setup = true;
-                SetupImage();
-            }
             if ((UnityEngine.Input.touchCount > 0 && UnityEngine.Input.GetTouch(0).phase == TouchPhase.Began)
                 || UnityEngine.Input.GetMouseButtonDown(0))
             {
-                if(displayed)
-                {
-                    displayed = false;
-                    maskObj.SetActive(false);
-                    File.Delete(GetPath(0));
-                }
-                else
-                {
-                    StartCoroutine(TakeScreenshot(0));
-                    Debug.Log("screenshot");
-                }
-            }
-            else
-            {
-                if (System.IO.File.Exists(GetPath(0)) && !displayed)
-                {
-                    Debug.Log("display");
-                    DisplayScreenshot(0);
-                    displayed = true;
-                }
+                //StartCoroutine(TakeScreenshot(0));
             }
         }
 
-        private string GetPath(/*Room room, */int index) => $"{Application.persistentDataPath}{index}.png"; // TODO: add room name
+        /// <summary>
+        /// Returns a file path based on the given room and index of the anchorpoint.
+        /// </summary>
+        /// <param name="room">The room that the screenshot corresponds with.</param>
+        /// <param name="index">The number of the anchorpoint that the screenshot corresponds with.</param>
+        /// <returns>A string representing the file path.</returns>
+        private string GetPath(Room room, int index) => $"{Application.persistentDataPath}{index}.png"; // TODO: add room name
 
-        private IEnumerator TakeScreenshot(/*Room room, */int index)
+        /// <summary>
+        /// Takes a screenshot for the given room with the anchorpoint's index
+        /// at the end of the frame and saves it locally.
+        /// </summary>
+        /// <param name="room">The room that the screenshot corresponds with.</param>
+        /// <param name="index">The number of the anchorpoint that the screenshot corresponds with.</param>
+        private IEnumerator TakeScreenshot(Room room, int index)
         {
             yield return new WaitForEndOfFrame();
 
-
             Texture2D screenshot = ScreenCapture.CaptureScreenshotAsTexture();
-            System.IO.File.WriteAllBytes(GetPath(/*room, */0), screenshot.EncodeToPNG());
-
-            // TODO: display text: screenshot taken
+            System.IO.File.WriteAllBytes(GetPath(room, index), screenshot.EncodeToPNG());
         }
 
-        private void DisplayScreenshot(/*Room room, */int index)
+        /// <summary>
+        /// Displays the specified screenshot on the screen.
+        /// </summary>
+        /// <param name="room">The room that the screenshot corresponds with.</param>
+        /// <param name="index">The number of the anchorpoint that the screenshot corresponds with.</param>
+        /// <param name="transparent">Whether the image displaying the screenshot should be transparent.</param>
+        private void DisplayScreenshot(Room room, int index, bool transparent = false)
         {
-            //image.gameObject.SetActive(true);
+            // set the images color either to transparent or solid
+            Color color = image.color;
+            if (transparent)    color.a = 0.4f;
+            else                color.a = 1;
+            image.color = color;
+
             maskObj.SetActive(true);
-            image.sprite = RetrieveScreenshot(GetPath(/*room, */index));
+            image.sprite = RetrieveScreenshot(GetPath(room, index));
         }
 
+        /// <summary>
+        /// Hides the screenshot being displayed on the screen.
+        /// </summary>
+        private void HideScreenshot()
+        {
+            image.sprite = null;
+            maskObj.SetActive(false);
+        }
+
+        /// <summary>
+        /// Deletes the specified screenshot.
+        /// </summary>
+        /// <param name="room">The room that the screenshot corresponds with.</param>
+        /// <param name="index">The number of the anchorpoint that the screenshot corresponds with.</param>
+        private void DeleteScreenshot(Room room, int index)
+        {
+            File.Delete(GetPath(room, index));
+        }
+
+        /// <summary>
+        /// Retrieves the screenshot from local memory at the given file path.
+        /// </summary>
+        /// <param name="filePath">The path where the screenshot is located.</param>
+        /// <returns>The screenshot.</returns>
         private Sprite RetrieveScreenshot(string filePath)
         {
             if (System.IO.File.Exists(filePath))
             {
                 byte[] bytes = System.IO.File.ReadAllBytes(filePath);
-                Texture2D texture = new Texture2D(1, 1);
+                Texture2D texture = new(1, 1);
                 texture.LoadImage(bytes);
                 return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
             }
             throw new Exception("Screenshot cannot be found.");
-        }
-
-        private void ClearImage()
-        {
-            image.sprite = null;
-            //image.gameObject.SetActive(false);
         }
 
         private void SetupImage()
@@ -118,12 +129,11 @@ namespace AwARe.RoomScan
             mask.showMaskGraphic = false;
 
             // create an image object
-            GameObject imageObj = new GameObject("ScreenshotImage");
+            GameObject imageObj = new("ScreenshotImage");
             imageObj.transform.parent = maskObj.transform;
             image = imageObj.AddComponent<Image>();
 
             // set image size to match screensize
-            //maskImage.rectTransform.pivot = new Vector2(0, 0);
             float maskSize = Screen.width / 1.5f;
             maskImage.rectTransform.sizeDelta = new Vector2(maskSize, maskSize);
             maskImage.rectTransform.localPosition = new Vector3(0, 0, 0);
@@ -131,19 +141,23 @@ namespace AwARe.RoomScan
 
             image.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
 
-            // make the image transparent
-            Color color = image.color;
-            color.a = 0.5f;
-            image.color = color;
-
-            GameObject outlineImageObj = new GameObject("OutlineImage");
+            GameObject outlineImageObj = new("OutlineImage");
             outlineImageObj.transform.parent = canvas.transform;
             Image outlineImage = outlineImageObj.AddComponent<Image>();
             outlineImage.sprite = outlineSprite;
             outlineImage.rectTransform.sizeDelta = new Vector2(maskSize, maskSize);
             outlineImage.rectTransform.localPosition = new Vector3(0, 0, 0);
+        }
 
-            ClearImage();
+        /// <summary>
+        /// Checks whether the specified screenshot has been saved.
+        /// </summary>
+        /// <param name="room">The room that the screenshot corresponds with.</param>
+        /// <param name="index">The number of the anchorpoint that the screenshot corresponds with.</param>
+        /// <returns>Whether the specified screenshot has been saved.</returns>
+        bool ScreenshotSaved(Room room, int index)
+        {
+            return System.IO.File.Exists(GetPath(room, index));
         }
     }
 }
